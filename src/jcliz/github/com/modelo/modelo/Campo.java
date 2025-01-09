91da8f1bc1169ffe4ac78915256f5f877171db77
João Pedro Cardoso de Liz <joaopedrocliz@gmail.com>
2025-01-09 16:28:54 -0300

    Criação dos atributos e construtor de Campo

--- src/jcliz/github/com/modelo/modelo/Campo.java
+++ src/jcliz/github/com/modelo/modelo/Campo.java
@@ -1,4 +1,24 @@
 package jcliz.github.com.modelo.modelo;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Campo {
+    private final int linha;
+    private final int coluna;
+
+    private boolean aberto;
+    private boolean minado;
+    private boolean marcado;
+
+    private List<Campo> vizinhos = new ArrayList<>();
+
+    public Campo(int linha, int coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    public boolean adicionarVizinho(Campo vizinho) {
+        //aplicar a lógica
+    }
 }
