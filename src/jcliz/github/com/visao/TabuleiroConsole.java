b9214c88e99525b98d121df6b80760bb5e1d3136
João Pedro Cardoso de Liz <joaopedrocliz@gmail.com>
2025-01-15 11:06:25 -0300

    Criação do método executarJogo

--- src/jcliz/github/com/visao/TabuleiroConsole.java
+++ src/jcliz/github/com/visao/TabuleiroConsole.java
@@ -11,3 +11,34 @@ import java.util.Scanner;
 public class TabuleiroConsole {
     private Tabuleiro tabuleiro;
     private Scanner leitor = new Scanner(System.in);
+
+    public TabuleiroConsole(Tabuleiro tabuleiro) {
+        this.tabuleiro = tabuleiro;
+
+        executarJogo();
+    }
+
+    private void executarJogo() {
+        try {
+            boolean continuar = true;
+
+            while (continuar) {
+                cicloDoJogo();
+
+                System.out.println("Outra partida? (S/n) ");
+                String resposta = leitor.nextLine();
+
+                if ("n".equalsIgnoreCase(resposta)) {
+                    continuar = false;
+                } else {
+                    tabuleiro.reiniciar();
+                }
+            }
+
+        } catch (SairException e) {
+            System.out.println("Fechando o jogo, até mais!");
+        } finally {
+            leitor.close();
+        }
+    }
+}
